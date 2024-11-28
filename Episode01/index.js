b4d8a42c6a54d4749e8aa3b7f59a023f03f4872a
Vaibhav Chitransh <110785035+vaibhav21212121@users.noreply.github.com>
2024-11-28 18:10:15 +0530

    episode01

---
+++ Episode01/index.js
@@ -0,0 +1,16 @@
+// Tempalte literals
+
+const age = 20;
+const fullname = "Vaibhav chitransh";
+
+//output
+
+console.log("Hello Boss! "+fullname+" how are you !!");
+console.log(`Hello Mr ${fullname} your age is :${age}`);
+
+
+// Print sum of two numbers
+
+const a = 10;
+const b =20;
+console.log(`Sum of two numbers ${a} and ${b} is : ${a+b} and ${Math.random(20)}`);
